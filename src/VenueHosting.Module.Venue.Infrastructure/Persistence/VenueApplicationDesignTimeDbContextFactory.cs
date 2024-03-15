9dfdc86ee517de5888a91aa47cf49046391b95ed
Mykola Kushnarenko <nikokusha@gmail.com>
2024-03-15 18:02:47 +0200

    Add VenueApplicationDesignTimeDbContextFactory

---
+++ src/VenueHosting.Module.Venue.Infrastructure/Persistence/VenueApplicationDesignTimeDbContextFactory.cs
@@ -0,0 +1,32 @@
+using Component.Persistence.SqlServer;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace VenueHosting.Module.Venue.Infrastructure.Persistence;
+
+internal class VenueApplicationDesignTimeDbContextFactory : IDesignTimeDbContextFactory<VenueApplicationDbContext>
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public VenueApplicationDesignTimeDbContextFactory()
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.local.json", true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        _serviceProvider = new ServiceCollection()
+            .AddDomainDbContext<VenueApplicationDbContext>(configuration)
+            .BuildServiceProvider();
+    }
+
+    public VenueApplicationDbContext CreateDbContext(string[] args)
+    {
+        var context = _serviceProvider.GetRequiredService<VenueApplicationDbContext>();
+
+        context.Database.EnsureCreated();
+
+        return context;
+    }
+}
