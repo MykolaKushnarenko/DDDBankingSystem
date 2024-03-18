c35750d8b23b8662c112d4077b6e979ae9bf117c
Mykola Kushnarenko <nikokusha@gmail.com>
2024-03-18 21:52:53 +0200

    Introduce ``Validation`` Component

---
+++ src/Components/Components.Validation/Ensure.cs
@@ -0,0 +1,14 @@
+﻿namespace Components.Validation;
+
+public static class Ensure
+{
+    public static T NotNull<T>(T value, string parameterName)
+    {
+        if (value is null)
+        {
+            throw new ArgumentNullException(parameterName);
+        }
+
+        return value;
+    }
+}
